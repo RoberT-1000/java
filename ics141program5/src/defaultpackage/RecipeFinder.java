8f9328f6f49c0aadd5a9438d89be109085616bd5
RoberT-1000 <RoberT-1000@users.noreply.github.com>
2017-07-08 11:50:21 -0500

    Update RecipeFinder.java

--- ics141program5/src/defaultpackage/RecipeFinder.java
+++ ics141program5/src/defaultpackage/RecipeFinder.java
@@ -1,3 +1,51 @@
 /*
  * this is the driver class
  */
+import java.io.*;
+
+public class RecipeFinder {
+	/*
+	 * instance variables
+	 */
+	private String recipe;
+	private String ingredients;
+	private BufferedReader br;
+	
+	/*
+	 * class methods
+	 */
+	
+	// constructor method
+	public RecipeFinder() {
+
+	}
+	
+	// setting value for recipe
+	public void setRecipe (String recipe) {
+		this.recipe = recipe;		
+	}
+	
+	// reading value for the 
+	public String readIngredients() {
+		String str;
+		try (BufferedReader br = new BufferedReader(new FileReader("recipefile.txt"))) {
+			// reading until we get #this.recipe
+			try {
+				while ((str = br.readLine()) != null) {
+					System.out.println(str);
+					if (str.equals("#" + this.recipe)) {
+						this.ingredients = br.readLine();
+						br.close();
+						return (this.ingredients);
+					}
+				} 
+				return ("Recipe could not be found.");
+			} catch (IOException recipenotfound) {
+				return ("Recipe " + this.recipe + " could not be found.");
+			}
+		} catch (IOException filenotfound) {
+			// TODO Auto-generated catch block
+			return ("File could not be found.");
+		}
+	} 
+}

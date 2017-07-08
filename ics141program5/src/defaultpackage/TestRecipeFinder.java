2750ba680731ccd365ed80f3494727993ece139b
RoberT-1000 <RoberT-1000@users.noreply.github.com>
2017-07-08 11:50:00 -0500

    Update TestRecipeFinder.java

--- ics141program5/src/defaultpackage/TestRecipeFinder.java
+++ ics141program5/src/defaultpackage/TestRecipeFinder.java
@@ -1,3 +1,34 @@
 /*
  * this is the application class
  */
+
+import java.io.*;
+
+public class TestRecipeFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String recipe = "";
+		
+		RecipeFinder myRecipe = new RecipeFinder();
+		
+		System.out.println("Welcome to Chinese recipe search program.");
+		System.out.println("Enter 'stop' to end.");
+
+		while (!((recipe.equals("stop")))) {
+			InputStreamReader instream = new InputStreamReader(System.in);
+			BufferedReader ins = new BufferedReader(instream);
+			System.out.print("Enter a recipe name: ");
+			try {
+				recipe = ins.readLine();
+				if (recipe.equals("stop")) { break; }
+				System.out.println(myRecipe.readIngredients());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Invalid input.");
+			}
+		}
+		System.out.println("Bye");
+	}
+
+}

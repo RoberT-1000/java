7ad262fb949fd1612f6fc30f1d4927bbb38c3670
RoberT-1000 <RoberT-1000@users.noreply.github.com>
2017-07-08 12:41:23 -0500

    Update RecipeFinder.java

--- ics141program5/src/defaultpackage/RecipeFinder.java
+++ ics141program5/src/defaultpackage/RecipeFinder.java
@@ -8,41 +8,52 @@ public class RecipeFinder {
 	private String recipe;
 	private String ingredients;
 	private BufferedReader br;
+	private String filePath;
+	private String fileName;
 	
 	/*
 	 * class methods
 	 */
 	
-	// constructor method
+	/*
+	 * constructor method...initializes the value of two Strings...
+	 * 	- filePath
+	 * 	- fileName
+	 */
 	public RecipeFinder() {
-
+		// assigns value to filePath
+		this.filePath = "C:\\Users\\" + System.getProperty("user.name") + "\\Documents\\recipefile\\";
+		this.fileName = "recipefile.txt";
+		System.out.println(this.filePath + this.fileName);
 	}
 	
-	// setting value for recipe
+	// setting value for recipe, input in TestRecipeFinder and passed as an argument here
 	public void setRecipe (String recipe) {
 		this.recipe = "#" + recipe;		
 	}
 	
-	// reading value for the 
-	public String readIngredients() {
+	/*
+	 * reading the ingredients or outputting an error that the recipe could not be found
+	 */
+	public void getIngredients() {
 		String str;
-		try (BufferedReader br = new BufferedReader(new FileReader("recipefile.txt"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(this.filePath + this.fileName))) {
 			// reading until we get #this.recipe
 			try {
 				do {
 					str = br.readLine();
 					if (str.equals(this.recipe)) {
 						this.ingredients = br.readLine();
-						return this.ingredients;
+						System.out.println(this.ingredients);
 					}
 				} while (str != null);
-				return ("Recipe could not be found.");
-			} catch (IOException recipenotfound) {
-				return ("Recipe " + this.recipe + " could not be found.");
+			} catch (NullPointerException recipenotfound) {
+				System.out.println("Recipe not found.");
 			}
 		} catch (IOException filenotfound) {
 			// TODO Auto-generated catch block
-			return ("File could not be found.");
+			System.out.println("File could not be found.");
+			System.exit(-1);
 		}
 	} 
 }
